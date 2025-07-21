409d0a822496d511ed18938df664704398f1a6d2
Abhijit <abhijitawari1402@gmail.com>
2025-07-21 20:28:36 +0530

    Day 5: Setup venv, installed Streamlit, created Hello World app

---
+++ week1/streamlit_hello.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Hello, Generative AI World! 👋")
+
+st.write("Welcome to my first Streamlit app.")
+st.write("We’re learning how to build Generative AI and Automation tools step by step.")
